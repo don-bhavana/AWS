821c1102f4870a1569f0b5ebda4c97f9b5bc334c
Mili <don.bhavana@gmail.com>
2020-07-22 12:18:37 +0530

    added excel parsing

--- ICV_api.py
+++ ICV_api.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
+import pymongo
 import flask
 from flask import jsonify, request
-from bson.json_util import dumps
 import openpyxl
-
+from datetime import datetime
 
 XLS_CELL_ADDRESS = [
 # COMPANY GENERAL INFORMATION
@@ -22,27 +22,132 @@ XLS_CELL_ADDRESS = [
 					'D21', #Financial Year End date (mm/dd/yyyy)
 					'G21', #Audited Financial Statement Issue date (mm/dd/yyyy)
 					'D24'#Company Business drop down list:[], selected value
-#IN-COUNTRY VALUE DETAILS
 ]
 
-wb = openpyxl.load_workbook("ICV-certificate-filled.xlsx", data_only=True)
-sheet = wb["Page 1 - ICV Summary"]
-x = "C10"
-for i in XLS_CELL_ADDRESS:
-	print(sheet[i].value)
+def create_dict():
+    wb = openpyxl.load_workbook("ICV-certificate-filled.xlsx", data_only=True)
+    sheet = wb["Page 1 - ICV Summary"]
+    page = {}
+    page['label'] = 'Page 1 - ICV Summary'
+    page['Company general information'] = {}
+    
+    page['Company general information']['Name (as per License)'] = {}
+    page['Company general information']['Name (as per License)']['value'] = sheet[XLS_CELL_ADDRESS[0]].value
+    page['Company general information']['Name (as per License)']['type'] = 'string'
+    page['Company general information']['Name (as per License)']['validated'] = 'boolean'
+    page['Company general information']['Name (as per License)']['comments'] = 'string'
+
+
+    page['Company general information']['License No'] = {}
+    page['Company general information']['License No']['value'] = sheet[XLS_CELL_ADDRESS[1]].value
+    page['Company general information']['License No']['type'] = 'string'
+    page['Company general information']['License No']['validated'] = 'boolean'
+    page['Company general information']['License No']['comments'] = 'string'
+
+
+    page['Company general information']['Address (as per License)'] = {}
+    page['Company general information']['Address (as per License)']['line1'] = {}
+    page['Company general information']['Address (as per License)']['line1']['value'] = sheet[XLS_CELL_ADDRESS[2]].value
+    page['Company general information']['Address (as per License)']['line1']['type'] = 'string'
+
+    page['Company general information']['Address (as per License)']['line2'] = {}
+    page['Company general information']['Address (as per License)']['line2']['value'] = sheet[XLS_CELL_ADDRESS[3]].value
+    page['Company general information']['Address (as per License)']['line2']['type'] = 'string'
+    
+    page['Company general information']['Address (as per License)']['line3'] = {}
+    page['Company general information']['Address (as per License)']['line3']['value'] = sheet[XLS_CELL_ADDRESS[4]].value
+    page['Company general information']['Address (as per License)']['line3']['type'] = 'string'
+    
+    page['Company general information']['Address (as per License)']['line4'] = {}
+    page['Company general information']['Address (as per License)']['line4']['value'] = sheet[XLS_CELL_ADDRESS[5]].value
+    page['Company general information']['Address (as per License)']['line4']['type'] = 'string'
+
+    page['Company general information']['Address (as per License)']['validated'] = 'boolean'
+    page['Company general information']['Address (as per License)']['comments'] = 'string'
+
+    page['Company general information']['Contact Person Details'] = {}
+    page['Company general information']['Contact Person Details']['Name'] = {}
+    page['Company general information']['Contact Person Details']['Name']['value'] = sheet[XLS_CELL_ADDRESS[6]].value
+    page['Company general information']['Contact Person Details']['Name']['type'] = 'string'
+    page['Company general information']['Contact Person Details']['Name']['validated'] = 'boolean'
+    page['Company general information']['Contact Person Details']['Name']['comments'] = 'string'
+
+    page['Company general information']['Contact Person Details']['E-mail'] = {}
+    page['Company general information']['Contact Person Details']['E-mail']['value'] = sheet[XLS_CELL_ADDRESS[7]].value
+    page['Company general information']['Contact Person Details']['E-mail']['type'] = 'string'
+    page['Company general information']['Contact Person Details']['E-mail']['validated'] = 'boolean'
+    page['Company general information']['Contact Person Details']['E-mail']['comments'] = 'string'
+
+    page['Company general information']['Contact Person Details']['Office'] = {}
+    page['Company general information']['Contact Person Details']['Office']['value'] = sheet[XLS_CELL_ADDRESS[8]].value
+    page['Company general information']['Contact Person Details']['Office']['type'] = 'string'
+    page['Company general information']['Contact Person Details']['Office']['validated'] = 'boolean'
+    page['Company general information']['Contact Person Details']['Office']['comments'] = 'string'
+
+    page['Company general information']['Contact Person Details']['Mobile'] = {}
+    page['Company general information']['Contact Person Details']['Mobile']['value'] = sheet[XLS_CELL_ADDRESS[9]].value
+    page['Company general information']['Contact Person Details']['Mobile']['type'] = 'Number'
+    page['Company general information']['Contact Person Details']['Mobile']['validated'] = 'boolean'
+    page['Company general information']['Contact Person Details']['Mobile']['comments'] = 'string'
+
+    page['Company general information']['Company Type'] = {}
+    page['Company general information']['Company Type']['value'] = sheet[XLS_CELL_ADDRESS[10]].value
+    page['Company general information']['Company Type']['Dropdown list'] = ['SME in UAE', 'Non SME in UAE', 'International Company']
+    page['Company general information']['Company Type']['type'] = 'Dropdown'
+    page['Company general information']['Company Type']['validated'] = 'boolean'
+    page['Company general information']['Company Type']['comments'] = 'string'
+
+    
+    page['Company general information']['Company based in'] = {}
+    page['Company general information']['Company Type']['value'] = sheet[XLS_CELL_ADDRESS[11]].value
+    page['Company general information']['Company Type']['Dropdown list'] = ['Outside UAE', 'Within UAE']
+    page['Company general information']['Company Type']['type'] = 'dropdown'
+    page['Company general information']['Company Type']['validated'] = 'boolean'
+    page['Company general information']['Company Type']['comments'] = 'string'
+
+
+    page['Company general information']['Financial Year End date (mm/dd/yyyy)'] = {}
+    page['Company general information']['Financial Year End date (mm/dd/yyyy)']['value'] = (sheet[XLS_CELL_ADDRESS[12]].value).isoformat()
+    page['Company general information']['Financial Year End date (mm/dd/yyyy)']['type'] = 'datetime'
+    page['Company general information']['Financial Year End date (mm/dd/yyyy)']['validated'] = 'boolean'
+    page['Company general information']['Financial Year End date (mm/dd/yyyy)']['comments'] = 'string'
+
+
+    page['Company general information']['Audited Financial Statement Issue date (mm/dd/yyyy)'] = {}
+    page['Company general information']['Audited Financial Statement Issue date (mm/dd/yyyy)']['value'] = (sheet[XLS_CELL_ADDRESS[12]].value).isoformat()
+    page['Company general information']['Audited Financial Statement Issue date (mm/dd/yyyy)']['type'] = 'datetime'
+    page['Company general information']['Audited Financial Statement Issue date (mm/dd/yyyy)']['validated'] = 'boolean'
+    page['Company general information']['Audited Financial Statement Issue date (mm/dd/yyyy)']['comments'] = 'string'
+
+    page['Company general information']['Company Business'] = {}
+    page['Company general information']['Company Business']['value'] = sheet[XLS_CELL_ADDRESS[11]].value
+    page['Company general information']['Company Business']['Dropdown list'] = ['GOOD MANUFACTURER', 'SERVICE PROVIDER']
+    page['Company general information']['Company Business']['type'] = 'dropdown'
+    page['Company general information']['Company Business']['validated'] = 'boolean'
+    page['Company general information']['Company Business']['comments'] = 'string'
+
+    
+    
+    return page
 
 
 client = MongoClient('mongodb://localhost:27017/')
-db = client.pymongo_test #db
-posts = db.posts # collection
+db = client.icv_db #db
+icv = db.icv # collection
+
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    scotts_posts = posts.find({'author': 'Scott'})
-    post = dumps(scotts_posts)
-   
-    return jsonify(post)
 
-app.run()
+    post = create_dict()
+    result = icv.insert_one(post)
+    id = result.inserted_id
+    
+    posts = list(icv.find({'_id': id}))
+    for record in posts:
+        record['_id'] = str(record.pop('_id'))
+    return jsonify(posts), 200
+
+app.run(host="0.0.0.0")
 
